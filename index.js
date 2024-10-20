6dc765b3fb7645773e628d3d7089a2ab5296d322
Carolina125 <crobenitez125@gmail.com>
2024-10-20 14:04:06 -0300

    JavaScript update

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "obcc5d4b0bc4t336609c2f30ba544fa3";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(updateWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
+
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#weather-search");
-
 searchElement.addEventListener("submit", searchSubmit);
